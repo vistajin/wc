b4d6a15b6d722e16ecc130f709dcd839b5ae2775
vistajin <528189@qq.com>
2017-12-13 17:42:40 +0800

    Create column-selection.js

---
+++ reference/column-selection.js
@@ -0,0 +1,6 @@
+var GameScore = Parse.Object.extend("GameScore");
+var query = new Parse.Query(GameScore);
+query.select("score", "playerName");
+query.find().then(function(results) {
+  // each of results will only have the selected fields available.
+});
