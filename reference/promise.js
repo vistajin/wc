e8046b047cc66194a2ac990add6706d5665f17a5
vistajin <528189@qq.com>
2017-12-15 17:48:14 +0800

    Update promise.js

--- reference/promise.js
+++ reference/promise.js
@@ -17,3 +17,23 @@ query.find().then(function(results) {
 }).then(function() {
   // Every comment was deleted.
 });
+
+
+
+
+var query = new Parse.Query("Comments");
+query.equalTo("post", post);
+
+query.find().then(function(results) {
+  // Collect one promise for each delete into an array.
+  var promises = [];
+  _.each(results, function(result) {
+    // Start this delete immediately and add its promise to the list.
+    promises.push(result.destroy());
+  });
+  // Return a new promise that is resolved when all of the deletes are finished.
+  return Parse.Promise.when(promises);
+
+}).then(function() {
+  // Every comment was deleted.
+});
