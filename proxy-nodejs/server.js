94814bf234a6a82c210e790d4cdb5b984bce7093
vistajin <528189@qq.com>
2017-12-17 21:55:52 +0800

    update for role/user management

--- proxy-nodejs/server.js
+++ proxy-nodejs/server.js
@@ -195,44 +195,300 @@ reqPost.on('error', function(e) {
 });
 */
 
-app.post('/user', (req, res) => {
+app.post('/role', (req, res) => {
   if (!checkLogin(req, res)) {
     return;
   }
 
-  var user = new Parse.User();
-  user.set("username", req.body.username);
-  user.set("password", req.body.password);
-  user.set("email", req.body.email);
-  user.set("phone", req.body.phone);
-
-  user.signUp(null, {
+  console.log("start to create role: ", req.body);
+  var roleACL = new Parse.ACL();
+  roleACL.setPublicReadAccess(false);
+  roleACL.setPublicWriteAccess(false);
+  var role = new Parse.Role(req.body.rolename, roleACL);
+  role.save(null, {
     useMasterKey: true,
-    success: function(user) {
-      console.log("register user successfully: ", req.body);
-      res.json(
+    success: function(role) {
+      console.log('Create role successfully: ', role);
+      res.status(200).json(
       {
         code: 0,
-        msg: "register user successfully"
+        msg: "Create role successfully"
       });
+
     },
-    error: function(user, error) {
-      // Show the error message somewhere and let the user try again.
-      console.error("Error: " + error.code + " " + error.message);
-      res.json(
+    error: function(role, error) {
+      console.log('Failed to create role', error);
+      res.status(400).json(
       {
-        code: 10002,
-        msg: "Failed to register user: " + error
+        code: 10003,
+        msg: "Failed to create role: " + error
+      });
+    }
+  });
+
+  //
+  //var administrators = /* Your "Administrators" role */;
+  //var moderators = /* Your "Moderators" role */;
+  //moderators.getRoles().add(administrators);
+  //moderators.save();
+
+});
+
+app.post('/roles/append', (req, res) => {
+  checkAdmin(req, res, function(isAdmin) {
+    if (isAdmin) {
+      // check if parent role exists
+      getRoleByName(req.body.parentRoleName, function(error, parentRole) {
+        if (error) {
+          res.status(400).json({
+            code: error.code,
+            msg: error.message
+          });
+          return;
+        }
+        // parent role exists, check if child role exists
+        getRoleByName(req.body.childRoleName, function(error, childRole) {
+          if (error) {
+            res.status(400).json({
+              code: error.code,
+              msg: error.message
+            });
+            return;
+          }
+          console.log("ParentRole=", parentRole);
+          console.log("childRole=", childRole);
+          parentRole.getRoles().add(childRole);
+          parentRole.save(null, {
+            useMasterKey: true,
+            success: function(obj) {
+              res.status(200).json({
+                code: 0,
+                msg: "add role successfully"
+              });
+              console.log("add role " + childRole + " to " + parentRole + "successfully.");
+            },
+            error: function(err) {
+              console.log("failed to add role " + childRole + " to " + parentRole, err);
+            }
+          });
+        });
       });
     }
   });
 });
 
-app.delete('/user', (req, res) => {
-  checkLogin(req, res);
+function addChildRole(parentRoleName, chileRoleName) {
+  var query = new Parse.Query(Parse.Role);
+  query.equalTo("name", req.params.name);
+  query.first({
+    useMasterKey: true,
+    success: function(object) {
+      object.getRoles().add();
+      res.status(200).json({
+        code: 0,
+        msg: object
+      });
+    },
+    error: function(error) {
+      console.error("Error: " + error.code + " " + error.message);
+      res.status(400).json({
+        code: error.code,
+        msg: error.message
+      });
+    }
+  });
+  var parentRole = new Parse.Role(parentRoleName);
+  parentRole.getRoles().add(chileRoleName);
+  parentRole.save(null, {
+    useMasterKey: true
+  });
+}
+
+// https://stackoverflow.com/questions/40381294/how-to-make-parse-find-save-operations-synchronous
+/*function getRoleByName(roleName) {
+  return new Promise(function(resolve, reject) {
+    var query = new Parse.Query(Parse.Role);
+    query.equalTo("name", roleName);
+    query.first({useMasterKey: true}).then(
+      function(object) {
+        if (object) {
+          resolve(object);
+        } else {
+          var error = new Parse.Error(10004, "Role not found");
+          console.error("getRoleByName: " + roleName + ", Error: " + error.code + " " + error.message);
+          reject(error);
+        }        
+      },
+      function(error) {
+        console.error("getRoleByName: " + roleName + ", Error: " + error.code + " " + error.message);
+        reject(error);
+      });  
+  });
+}*/
+function getRoleByName(roleName, callback) {
+  var query = new Parse.Query(Parse.Role);
+  query.equalTo("name", roleName);
+  query.first({useMasterKey: true}).then(
+    function(object) {
+      if (object) {
+        callback(null, object);
+      } else {
+        var error = new Parse.Error(10004, "Role not found");
+        console.error("getRoleByName: " + roleName + ", Error: " + error.code + " " + error.message);
+        callback(error);
+      }
+    },
+    function(error) {
+      console.error("getRoleByName: " + roleName + ", Error: " + error.code + " " + error.message);
+      callback(error);
+    });
+}
+
+app.get('/roles/:name', (req, res) => {
+  if (!checkLogin(req, res)) {
+    return;
+  }
   
+  getRoleByName(req.params.name, function(error, role) {
+    if (error) {
+      res.status(400).json({
+        code: error.code,
+        msg: error.message
+      });
+    } else {
+      res.status(200).json({
+        code: 0,
+        msg: role
+      });
+    }
+  });
+  /*getRoleByName(req.params.name)
+    .then(
+      function(role) {
+        res.status(200).json({
+          code: 0,
+          msg: role
+        });
+      })
+    .catch(
+      function(error) {
+        res.status(400).json({
+          code: error.code,
+          msg: error.message
+        });
+      });*/
 });
 
+app.post('/user/add-to-role', (req, res) => {
+  console.log("call /user/add-to-role: ", req.body);
+  checkAdmin(req, res, function(isAdmin) {
+    if (isAdmin) {
+      // check if role exists
+      getRoleByName(req.body.roleName, function(error, role) {
+        if (error) {
+          res.status(400).json({
+            code: error.code,
+            msg: error.message
+          });
+          return;
+        }
+        // role exists, check if user exists
+        getUserById(req.body.userId, function(error, user) {
+          if (error) {
+            res.status(400).json({
+              code: error.code,
+              msg: error.message
+            });
+            return;
+          }
+          // user exists, add user to role
+          role.getUsers().add(user);
+          role.save(null, {useMasterKey: true}).then(
+            function(obj) {
+              res.status(200).json({
+                code: 0,
+                msg: "add user " + req.body.userId + " to role " + req.body.roleName + " successfully"
+              });
+              console.log("add user " + req.body.userId + " to role " + req.body.roleName + " successfully");
+            },
+            function(err) {
+              res.status(400).json({
+                code: 10007,
+                msg: "failed to add user " + req.body.userId + " to role " + req.body.roleName
+              });
+              console.error("failed to add user " + req.body.userId + " to role " + req.body.roleName, err);
+            }
+          );
+        });
+      });
+    }
+  });
+});
+
+function getUserById(userId, callback) {
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("objectId", userId);
+  query.first({useMasterKey: true}).then(
+    function(object) {
+      if (object) {
+        callback(null, object);
+      } else {
+        var error = new Parse.Error(10006, "User not found by ID: " + userId);
+        console.error("getUserById: " + userId + ", Error: " + error.code + " " + error.message);
+        callback(error);
+      }
+    },
+    function(error) {
+      console.error("getUserById: " + userId + ", Error: " + error.code + " " + error.message);
+      callback(error);
+    });
+}
+
+app.post('/user', (req, res) => {
+  checkAdmin(req, res, function(isAdmin) {
+    if (isAdmin) {
+      console.log("start to register user: ", req.body);
+      var user = new Parse.User();
+      user.set("username", req.body.username);
+      user.set("password", req.body.password);
+      user.set("email", req.body.email);
+      user.set("phone", req.body.phone);
+
+      user.signUp(null, {
+        useMasterKey: true,
+        success: function(user) {
+          console.log("register user successfully: ", req.body);
+          res.status(200).json(
+          {
+            code: 0,
+            msg: "register user successfully"
+          });
+        },
+        error: function(user, error) {
+          // Show the error message somewhere and let the user try again.
+          console.error("Error: " + error.code + " " + error.message);
+          res.status(400).json(
+          {
+            code: 10002,
+            msg: "Failed to register user: " + error
+          });
+        }
+      });
+    }
+  });  
+});
+
+app.delete('/user', (req, res) => {
+  checkAdmin(req, res, function(isAdmin) {
+    if (isAdmin) {
+      res.status(200).json(
+      {
+        code: 0,
+        msg: "Is admin: " + req.session.user
+      });
+    }
+  });
+});
 
 app.post('/login', (req, res) => {
   // console.log(">>>>>>" + req.session.user);
@@ -250,16 +506,17 @@ app.post('/login', (req, res) => {
       useMasterKey: true,
       success: function(user) {
         console.log("logon successfully!", user);
-        req.session.user = {
+        req.session.user = user;
+        /*{
           name: user.getUsername(),
           logonTime: new Date(),
           sessionId: user.getSessionToken()
           // TODO: to add more valuable fields
-        };
+        };*/
         console.log("save to session:", req.session.user);
 
         //res.redirect("/home");
-        res.json(
+        res.status(200).json(
           {
             code: 0,
             msg: "logon successfully"
@@ -269,7 +526,7 @@ app.post('/login', (req, res) => {
       error: function(user, error) {
         console.error("Failed to logon", user.getUsername(), ":" , error);
         //res.redirect("/error");
-        res.json(
+        res.status(400).json(
           {
             code: 999,
             msg: error
@@ -290,7 +547,7 @@ app.delete('/logout', (req, res) => {
       }      
     });
     //res.redirect("/home");
-    res.json(
+    res.status(200).json(
       {
         code: 0,
         msg: "logout successfully"
@@ -298,7 +555,7 @@ app.delete('/logout', (req, res) => {
     );
   } else {
     console.log("User not yet login.");
-    res.json(
+    res.status(400).json(
       {
         code: 999,
         msg: "User not yet login."
@@ -308,8 +565,10 @@ app.delete('/logout', (req, res) => {
 });
 
 function checkLogin(req, res) {
+  return true;
+  /*
   if (!req.session.user) {
-    res.json(
+    res.status(400).json(
       {
         code: 10001,
         msg: "User not yet login."
@@ -317,12 +576,75 @@ function checkLogin(req, res) {
     );
     return false;
   }
-  return true;
+  return true;*/
 }
 
+function isUserInRole(roleName, userId, callback) {
+  getRoleByName(roleName, function(error, role) {
+    if (error) {      
+      return false;
+    } else {
+      var relation = new Parse.Relation(role, 'users');
+      var query = relation.query();
+
+      query.equalTo('objectId', userId);
+      query.first({useMasterKey: true}).then(
+        function(result) {
+          if (result) {
+            console.log("User " + userId + " is in role " + roleName);
+            callback(true);
+          } else {
+            console.log("User " + userId + " is NOT in role " + roleName);
+            callback(false);
+          }
+        },
+        function(err) {
+          console.log("isUserInRole error.", err);
+          callback(false, err);
+        }
+      );
+    }
+  });
+}
+
+function checkAdmin(req, res, callback) {
+  if (!req.session.user) {
+    res.status(400).json(
+      {
+        code: 10001,
+        msg: "User not yet login."
+      }
+    );
+    return false;
+  }
+  isUserInRole("admin", req.session.user.objectId, function(result, err) {
+    if (!result) {
+      if (err) {
+        res.status(400).json(
+          {
+            code: err.code,
+            msg: err.message
+          }
+        );
+      } else {
+        res.status(400).json(
+          {
+            code: 10005,
+            msg: "User not admin."
+          }
+        );
+      }
+    }
+    callback(result);
+  });
+}
 
 app.post("/role/create", (req, res) => {
-  res.json(
+  if (!checkLogin(req, res)) {
+    return;
+  }
+
+  res.status(200).json(
     {
       user: req.body.username,
       password: req.body.password
